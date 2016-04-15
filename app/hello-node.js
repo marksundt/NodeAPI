7bf51137ad5afab81b9f2b6c75b452f6ef10eb53
Mark Sundt <marksundt@gmail.com>
2016-04-14 22:10:57 -0500

    First working

--- app/hello-node.js
+++ app/hello-node.js
@@ -7,14 +7,15 @@ var port = 8180;
 
 function handle_request(request, response) {
     response.writeHead(200, {
-        'Content-Type' : 'text/plain'
+        'Content-Type': 'text/plain'
     });
 
-response.end('Hello World. Are you restless to go restful? \n');
+    response.end('Hello World. Are you restless to go restful? \n');
     console.log('requested');
-
- http.createServer(handle_reqeust).listen(port, '127.0.0.1');
-
- console.log('Started Node.js http server at http://127.0.0.1'+port);
-    
 }
+
+ http.createServer(handle_request).listen(port, '127.0.0.1');
+
+ console.log('Started Node.js http server at http://127.0.0.1:'+ port);
+
+
